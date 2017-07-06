666869c67ea90204862e53b2620fade20438fa45
Marco Bonelli <mb5.marcob@gmail.com>
2017-07-06 17:46:29 +0200

    Extracted utils.

---
+++ src/utils.c
@@ -0,0 +1,80 @@
+inline void* malloc_or_die(const size_t sz) {
+	void* mem;
+
+	mem = malloc(sz);
+	if (mem == NULL)
+		exit(1);
+
+	return mem;
+}
+
+inline void* calloc_or_die(size_t n, size_t sz) {
+	void* mem;
+
+	mem = calloc(n, sz);
+	if (mem == NULL)
+		exit(1);
+
+	return mem;
+}
+
+inline void* realloc_or_die(void* mem, size_t sz) {
+	mem = realloc(mem, sz);
+	if (mem == NULL)
+		exit(1);
+
+	return mem;
+}
+
+inline void* malloc_null(size_t n, size_t sz) {
+	void** mem;
+	size_t i;
+
+	if (0 == NULL) {
+		mem = calloc_or_die(n, sz);
+	} else {
+		mem = malloc_or_die(sz * n);
+		for (i = 0; i < n; i++)
+			mem[i] = NULL;
+	}
+
+	return mem;
+}
+
+int getdelims(char** restrict str, const char* delims, FILE* restrict stream) {
+	size_t str_size, str_len;
+	register int c;
+	char* cur;
+
+	str_len  = 0;
+	str_size = GETDELIMS_BUFFER_CHUNK_SIZE + 1;
+	*str     = malloc_or_die(str_size);
+	cur      = *str;
+
+	for (;;) {
+		c = getc(stream);
+
+		if (str_len == str_size) {
+			str_size += GETDELIMS_BUFFER_CHUNK_SIZE;
+			*str      = realloc_or_die(*str, str_size);
+			cur       = *str + str_len;
+		}
+
+		if (strchr(delims, c) != NULL)
+			break;
+
+		if (c == EOF) {
+			if (str_len == 0)
+				return -1;
+			break;
+		}
+
+		*cur = c;
+		cur++;
+		str_len++;
+	}
+
+	*cur = '\0';
+
+	return (int)str_len;
+}
---
+++ src/utils.h
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+#ifndef API_PROJECT_UTILS_INCLUDED
+#define API_PROJECT_UTILS_INCLUDED
+
+#define GETDELIMS_BUFFER_CHUNK_SIZE 128
+
+/**
+ * Wrapper of malloc which forces the program to exit in case of failure.
+ */
+inline void* malloc_or_die(size_t);
+
+/**
+ * Wrapper of calloc which forces the program to exit in case of failure.
+ */
+inline void* calloc_or_die(size_t, size_t);
+
+/**
+ * Wrapper of realloc which forces the program to exit in case of failure.
+ */
+inline void* realloc_or_die(void*, size_t);
+
+/**
+ * Wrapper of malloc_or_die/calloc_or_die which allocates an array of NULL pointers.
+ */
+inline void* malloc_null(size_t, size_t);
+
+/**
+ * Read from the provided stream into the provided string variable until any delimiter or EOF is found, dynamically allocating the needed memory.
+ * @param str   : address of the array of chars where to put the string read.
+ * @param delims: string containing all the delimiter characters.
+ * @param stream: file stream to read from.
+ * @ret   number of characters read or -1 in case EOF was already reached.
+ * @post  *str contains the NUL-terminated string read.
+ */
+int getdelims(char** restrict, const char*, FILE* restrict);
+
+#endif
