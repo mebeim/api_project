55d94119faaf590b058a4d039d33e3748e784557
Marco Bonelli <mb5.marcob@gmail.com>
2017-07-25 21:13:38 +0200

    Added random generated tests to test script.

--- test/test_all.sh
+++ test/test_all.sh
@@ -2,7 +2,7 @@
 
 # File  : test_all.sh
 # Author: Marco Bonelli
-# Date  : 2017-07-20
+# Date  : 2017-07-25
 #
 # Copyright (c) 2017 Marco Bonelli.
 #
@@ -18,7 +18,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-printf "Running all tests:\n"
+function test_random {
+	res=0
+	printf "./random_fs.py -n %d | ../build/simplefs (x%d)... " $1 $2
+
+	for i in $(seq 1 $2); do
+		./random_fs.py -n $1 | ../build/simplefs > dummy_out
+		./random_fs.py -n $1 -o | cmp --quiet dummy_out -
+
+		if [ $? -ne 0 ]; then
+			res=$?
+			break
+		fi
+	done
+
+	rm dummy_out
+
+	if [ $res -eq 0 ]; then
+		printf "OK.\n"
+	else
+		printf "ERROR!\n\nTesting failed! :(\n"
+		exit 1
+	fi
+}
+
+printf "Running all test files:\n"
 
 i=0
 n=$(ls input -1 | wc -l)
@@ -37,5 +61,12 @@ for f in input/*.in; do
 		exit 1
 	fi
 done
+
+printf "\nRunning random tests:\n"
+test_random 10 100
+test_random 100 10
+test_random 1000 10
+test_random 10000 1
+test_random 100000 1
 
 printf "\nTesting succeded! :)\n"
